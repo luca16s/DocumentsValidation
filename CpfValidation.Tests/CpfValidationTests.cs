e8cc0882ff4cf603806b8455c4148c6b505cd174
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-03-28 15:08:34 -0300

    Creation of the CPF Validation Library Tool.

---
+++ CpfValidation.Tests/CpfValidationTests.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CpfValidation.Tests
+{
+    public class CpfValidationTests
+    {
+        private readonly CpfValidation cpfValidation;
+
+        public CpfValidationTests()
+        {
+            cpfValidation = new CpfValidation();
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_TRUE_QUANDO_INPUT_CONTER_LETRA()
+        {
+            var cpf = "1S114758941";
+
+            var result = cpfValidation.CheckCpfForLetters(cpf);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_FALSE_QUANDO_INPUT_NAO_CONTER_LETRA()
+        {
+            var cpf = "13114758941";
+
+            var result = cpfValidation.CheckCpfForLetters(cpf);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_FALSO_QUANDO_INPUT_FOR_VAZIO()
+        {
+            var cpf = "";
+
+            var result = cpfValidation.VerifyInputLenght(cpf);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_FALSE_QUANDO_INPUT_FOR_MAIOR_QUE_ONZE()
+        {
+            var cpf = "123456789101112";
+
+            var result = cpfValidation.VerifyInputLenght(cpf);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_TRUE_QUANDO_INPUT_FOR_IGUAL_ONZE()
+        {
+            var cpf = "12345678910";
+
+            var result = cpfValidation.VerifyInputLenght(cpf);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_FALSE_QUANDO_INPUT_FOR_DE_NUMEROS_INVALIDOS()
+        {
+            List<string> invalidNumbers = new List<string>
+            {
+                "00000000000",
+                "11111111111",
+                "22222222222",
+                "33333333333",
+                "44444444444",
+                "55555555555",
+                "66666666666",
+                "77777777777",
+                "88888888888",
+                "99999999999"
+            };
+
+            invalidNumbers.ForEach(cpf => Assert.False(cpfValidation.CheckFalseSequences(cpf)));
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_TRUE_QUANDO_INPUT_FOR_DE_NUMEROS_VALIDOS()
+        {
+            List<string> invalidNumbers = new List<string>
+            {
+                "46565053073",
+                "85776255082",
+                "19644564014",
+                "39584448064",
+                "29695291066",
+                "09505805039",
+                "31731015020",
+                "34614072089",
+                "13579555022",
+                "74025516012"
+            };
+
+            invalidNumbers.ForEach(cpf => Assert.True(cpfValidation.CheckFalseSequences(cpf)));
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_FALSE_QUANDO_PRIMEIRO_DIGITO_DO_CPF_FOR_INVALIDO()
+        {
+            var cpf = "19210363610";
+
+            var result = cpfValidation.CheckCpfVerifyingDigit(cpf);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_FALSE_QUANDO_SEGUNDO_DIGITO_DO_CPF_FOR_INVALIDO()
+        {
+            var cpf = "19210363621";
+
+            var result = cpfValidation.CheckCpfVerifyingDigit(cpf);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_TRUE_QUANDO_INPUT_DO_CPF_FOR_VALIDO()
+        {
+            var cpf = "19210363620";
+
+            var result = cpfValidation.CheckCpfVerifyingDigit(cpf);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TESTA_RETORNANDO_TRUE_QUANDO_INPUT_DO_CPF_FOR_VALIDO_COM_DIGITO_DEZ()
+        {
+            var cpf = "15382205710";
+
+            var result = cpfValidation.CheckCpfVerifyingDigit(cpf);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ CpfValidation/CpfValidation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CpfValidation
+{
+    public class CpfValidation
+    {
+        public bool VerifyInputLenght(string cpf)
+        {
+            return cpf.Length > 0 && cpf.Length < 11;
+        }
+
+        public bool CheckFalseSequences(string cpf)
+        {
+            var cpfArray = cpf.ToCharArray();
+
+            return cpfArray.GroupBy(x => x.ToString()).ToList().Count > 1;
+        }
+
+        public bool CheckCpfForLetters(string cpf)
+        {
+            return cpf.Any(c => !char.IsDigit(c));
+        }
+
+        public bool CheckCpfVerifyingDigit(string cpf)
+        {
+            return CheckFirstDigitIsValid(cpf) && CheckSecondDigitIsValid(cpf);
+        }
+
+        private bool CheckFirstDigitIsValid(string cpf)
+        {
+            var cpfArray = cpf.ToCharArray();
+
+            return (CalculateBaseDigits(cpfArray, 0, 9) * 10 % 11).ToString() == cpfArray[9].ToString();
+        }
+
+        private bool CheckSecondDigitIsValid(string cpf)
+        {
+            var cpfArray = cpf.ToCharArray();
+
+            return (CalculateBaseDigits(cpfArray, 1, 10) * 10 % 11).ToString() == cpfArray[10].ToString()
+                || (CalculateBaseDigits(cpfArray, 1, 10) * 10 % 11).ToString() == "10";
+        }
+
+        private double CalculateBaseDigits(char[] cpfArray, int startPosition, int size)
+        {
+            var count = 10;
+
+            double cpfValidationCalc = default;
+
+            for (int i = startPosition; i < size; i++)
+            {
+                cpfValidationCalc += double.Parse(cpfArray[i].ToString()) * count--;
+            }
+
+            return cpfValidationCalc;
+        }
+    }
+}
